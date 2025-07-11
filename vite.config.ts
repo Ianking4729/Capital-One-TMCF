c3a60c76131972f77a5976f2b4ede74a6c36eae3
unknown <Ian>
2025-07-10 21:08:54 -0400

    m

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Capital-One-TMCF/' : '/',
+  base: '/Capital-One-TMCF/',
   server: {
     host: "::",
     port: 8080,
